fb11d52df5f335567b46fca510e2a2b1e26a6f34
Mudit Kumar <mudit@victoriousinfotech.com>
2023-09-11 19:07:01 +0530

    cors origion add for res

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ const { Socket } = require("dgram");
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://tubular-kleicha-856a67.netlify.app",
+    // "https://localhost:5000"
+  })
+);
 const server = http.createServer(app);
 
 dotenv.config();
@@ -22,6 +29,7 @@ dotenv.config();
 const io = new Server(server, {
   cors: {
     origin: "https://tubular-kleicha-856a67.netlify.app",
+    // "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
@@ -135,7 +143,6 @@ io.on("connection", (socket) => {
     );
     socket.join(roomName);
     console.log(`User joined room: ${roomName}`);
-
     try {
       const sender = await userData.findOne({
         refrenceId: data.senderRefrenceId,
@@ -231,6 +238,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log("port 5000 activate");
 });
